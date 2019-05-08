d31905d5102aa13891a60e92b6f03aa0f37f95f6
Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
2019-05-08 09:26:11 -0700

    Remove custom.el from .gitignore, add config.el to .gitignore

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" default)))
+ '(package-selected-packages
+   (quote
+    (eyebrowse zoom alert direnv web-mode go-guru company-go go-eldoc go-mode yaml-mode company-tern rjsx-mode xref-js2 js2-refactor js2-mode elpy tide typescript-mode slime lispy alchemist helm-xref cquery flycheck restclient shell-pop rainbow-delimiters rainbow-mode smartparens company-lsp lsp-ui lsp-mode dockerfile-mode docker helm-tramp helm-ag git-timemachine magit helm-projectile projectile smart-indent-rigidly smartscan deadgrep iedit visual-regexp symbol-overlay wgrep move-dup multiple-cursors expand-region anzu hydra swiper which-key fasd dired-efap ace-window fancy-battery spaceline exec-path-from-shell use-package)))
+ '(zoom-size (quote (0.618 . 0.618))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
